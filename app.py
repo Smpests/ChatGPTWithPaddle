0b1dfc0972c0c5e3ad4fa19c0a17b072452c4c93
L1u <932044860@qq.com>
2023-04-28 10:19:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from paddleocr import PaddleOCR
 import gradio as gr
 from PIL import Image
 import numpy as np
+from paddlespeech.cli.tts.infer import TTSExecutor
 
 # 连接open ai
 chatGPT = Chatbot(api_key=os.environ["openai_api_key"])
@@ -13,6 +14,30 @@ chatGPT = Chatbot(api_key=os.environ["openai_api_key"])
 ocr = PaddleOCR(use_angle_cls=True, lang="ch")  # need to run only once to download and load model into memory
 
 MAX_FILE_SIZE = 3 * 1024 * 1024  # 单位字节，此处为2M
+
+# 加载paddlespeech模型
+audio_number = 0  # 语音文件编号
+tts = TTSExecutor()
+tts(
+    text="initailize model.",
+    output=f'output{audio_number}.wav',
+    am='fastspeech2_mix',
+    am_config=None,
+    am_ckpt=None,
+    am_stat=None,
+    spk_id=0,
+    phones_dict=None,
+    tones_dict=None,
+    speaker_dict=None,
+    voc='hifigan_ljspeech',
+    voc_config=None,
+    voc_ckpt=None,
+    voc_stat=None,
+    lang='mix',
+)  # 参数配置参见：https://github.com/PaddlePaddle/PaddleSpeech/blob/develop/demos/text_to_speech/README_cn.md
+# 发现目前每个TTSExecutor()配置参数后，不能再更改，需要重新创建一个对象
+# 另外cpu下极慢，需prompt说回答不要超过多少字
+audio_number += 1
 
 
 def add_text(history, text):
@@ -37,7 +62,7 @@ def add_file(history, file):
     return history
 
 
-def bot(history):
+def bot(history, use_speech=False):
     if isinstance(history[-1][0], (str,)):
         question = history[-1][0]
         if question.startswith("暂不支持"):
@@ -57,6 +82,43 @@ def bot(history):
         for data in chatGPT.ask_stream(question):
             history[-1][1] += data
             yield history
+        # 音频暂不支持stream，因此等输出完之后再生成
+        if use_speech and len(history[-1][1].split(" ")) < 100:
+            print("audio generating...")
+            global audio_number
+            tts(
+              text=history[-1][1],
+              output=f'output{audio_number}.wav',
+              am='fastspeech2_mix',
+              am_config=None,
+              am_ckpt=None,
+              am_stat=None,
+              spk_id=0,
+              phones_dict=None,
+              tones_dict=None,
+              speaker_dict=None,
+              voc='hifigan_ljspeech',
+              voc_config=None,
+              voc_ckpt=None,
+              voc_stat=None,
+              lang='mix',
+            )  
+            history += [(None, (f"output{audio_number}.wav", None))]
+            audio_number += 1
+            yield history
+
+
+prompts_dict = {
+    "None": "Please forget what I said before.",
+    "Teacher": "I want you to act as a spoken English teacher and improver. I will speak to you in English and you will reply to me in English to practice my spoken English. I want you to keep your reply neat, limiting the reply to 100 words. I want you to strictly correct my grammar mistakes, typos, and factual errors. I want you to ask me a question in your reply. Now let's start practicing, you could ask me a question first. Remember, I want you to strictly correct my grammar mistakes, typos, and factual errors.",
+}
+
+def change_prompt(history, label_name):
+    if label_name == "Spoken English Teacher":
+        history += [(prompts_dict["None"], None)]
+    else:
+        history += [(prompts_dict["Teacher"], None)]
+    return history
 
 
 with gr.Blocks() as demo:
@@ -71,10 +133,17 @@ with gr.Blocks() as demo:
         with gr.Column(scale=0.5, min_width=0):
             upload_btn = gr.UploadButton("📁", file_types=["image"])
     with gr.Row():
-        gr.Markdown("- OpenAI ChatGPT3.5模型，github开源PaddleOCR、acheong08/ChatGPT；\n- 注：上传的图片需带有文字描述的问题，类似小猿搜题。")
+        with gr.Column(scale=0.5, min_width=0):
+            use_speech = gr.Checkbox(label="Speech(slow on cpu)", info="Whether to use PaddleSpeech?")
+        with gr.Column(scale=0.5, min_width=0):
+            prompts = gr.Radio(["Spoken English Teacher", "None"], value="None", label="Prompt")
+    with gr.Row():
+        gr.Markdown("- OpenAI ChatGPT3.5模型，github开源PaddleOCR、acheong08/ChatGPT、PaddleSpeech；\n- 注：上传的图片需带有文字描述的问题，类似小猿搜题；\n- 语音输入请用手机上自带的语音识别，另外为节省时间，只会输出100个字以内的语音。")
 
-    txt.submit(add_text, [chatbot, txt], [chatbot, txt]).then(bot, chatbot, chatbot)
-    upload_btn.upload(add_file, [chatbot, upload_btn], [chatbot]).then(bot, chatbot, chatbot)
+
+    txt.submit(add_text, [chatbot, txt], [chatbot, txt]).then(bot, [chatbot, use_speech], chatbot)
+    upload_btn.upload(add_file, [chatbot, upload_btn], [chatbot]).then(bot, [chatbot, use_speech], chatbot)
     send_btn.click(add_text, [chatbot, txt], [chatbot, txt]).then(bot, chatbot, chatbot)
+    prompts.select(fn=change_prompt, inputs=[chatbot, prompts], outputs=[chatbot]).then(bot, [chatbot, use_speech], chatbot)
 
-demo.queue(concurrency_count=10).launch()
+demo.queue(concurrency_count=5).launch()
